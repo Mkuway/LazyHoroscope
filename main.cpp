c5772c58a8d9614cc16a2a2782c660cce4fb353b
Mkuway <mkuway@mail.csuchico.edu>
2018-09-19 17:35:58 -0700

    Takes the users name and gives them a nickname using their intials and gives back your lucky number using your first name length

--- main.cpp
+++ main.cpp
@@ -7,10 +7,18 @@ using namespace std;
 int main()
 {
   //declare variables
-
+    string first;
+    string last;
+    int lucky;
   //get user input
+    cout<<"What is your first name? ";
+    cin>>first;
 
+    cout<<"What is your last name? ";
+    cin>>last;
   //tell fortune
-
-  return 0;
+    cout<<"Welcome, " <<first[0]<<last[0]<<" here is your fortune..."<<endl;
+   // cout<<"The variable has "<<first.length()<<" letters"<<endl;
+    cout<<"Your lucky number is "<<first.length()<<endl; 
+    return 0;
 }
