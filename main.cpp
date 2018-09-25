0e6f1a9093b3a31e53ed05ef3c188677c9dd49f4
Mkuway <mkuway@mail.csuchico.edu>
2018-09-24 23:18:35 -0700

    Asks the user for full name, spits out a lucky number, and checks for vowels in  the first and last name

--- main.cpp
+++ main.cpp
@@ -13,13 +13,13 @@ int main()
     int index;
     index=last.length()-1;
   //get user input
-    cout<<"What is your first name? "<<endl;
+    cout<<"What is your first name?"<<endl;
     cin>>first;
 
-    cout<<"What is your last name? "<<endl;
+    cout<<"What is your last name?"<<endl;
     cin>>last;
   //tell fortune
-    cout<<"Welcome, " <<first[0]<<last[0]<<" here is your fortune..."<<endl;
+    cout<<"Welcome, " <<first[0]<<"."<<last[0]<<".,"<<" here is your fortune..."<<endl;
    // cout<<"The variable has "<<first.length()<<" letters"<<endl;
     cout<<"Your lucky number is "<<first.length()<<endl;
     if(first[0] == 'a' || first[0] == 'A' || first[0] == 'e' || first[0] == 'E' || first[0] == 'i' || first[0] == 'I' || first[0] == 'o' || first[0] == 'O' )
@@ -28,13 +28,13 @@ int main()
     }
     else
     {
-            cout<<"You should keep a low profile. "<<endl;
+            cout<<"you should keep a low profile."<<endl;
     }
     if(index == 'a' || index == 'A' || index == 'e' || index == 'E' || index == 'i' || index == 'I'|| index == 'o' || index == 'O')
     {
             cout<<"you have already me your true love. "<<endl;
     }
-    cout<<" Have a good day! "<<endl;
+    cout<<"have a good day! "<<endl;
 
     return 0;
 }
