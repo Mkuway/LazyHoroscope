f2e879e57fad29b7f133d86876ee70d059debaab
Mkuway <mkuway@mail.csuchico.edu>
2018-09-24 23:39:33 -0700

    Asks the user for full name, spits out a lucky number, and checks for vowels in  the first and last name

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@ int main()
     string first;
     string last;
     int lucky;
-    int index;
-    index=last.length()-1;
+   // int index;
+   // index == last.length()-1;
   //get user input
     cout<<"What is your first name?"<<endl;
     cin>>first;
@@ -22,15 +22,15 @@ int main()
     cout<<"Welcome, " <<first[0]<<"."<<last[0]<<".,"<<" here is your fortune..."<<endl;
    // cout<<"The variable has "<<first.length()<<" letters"<<endl;
     cout<<"your lucky number is "<<first.length()<<endl;
-    if(first[0] == 'a' || first[0] == 'A' || first[0] == 'e' || first[0] == 'E' || first[0] == 'i' || first[0] == 'I' || first[0] == 'o' || first[0] == 'O' )
+    if(first[0] == 'a' || first[0] == 'A' || first[0] == 'e' || first[0] == 'E' || first[0] == 'i' || first[0] == 'I' || first[0] == 'o' || first[0] == 'O' || first[0] == 'u' || first[0] == 'U')
     {
-            cout<<"you are destined to be famous! "<<endl;
+            cout<<"you are destined to be famous!"<<endl;
     }
     else
     {
             cout<<"you should keep a low profile."<<endl;
     }
-    if(index == 'a' || index == 'A' || index == 'e' || index == 'E' || index == 'i' || index == 'I'|| index == 'o' || index == 'O')
+    if(last.length()-1 == 'a' || last.length()-1 == 'A' || last.length()-1 == 'e' || last.length()-1 == 'E' || last.length()-1 == 'i' || last.length()-1 == 'I'|| last.length()-1 == 'o' || last.length()-1 == 'O' || last.length()-1 == 'u' || last.length()-1 == 'U')
     {
             cout<<"you have already me your true love. "<<endl;
     }
